8e0063c20177cbf02ef1efd0968d521d95d6a70b
sbi <giwinax@gmail.com>
2020-11-16 16:20:58 +0300

    init commit

--- CalcSur/CalcSurApp.swift
+++ CalcSur/CalcSurApp.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 @main
-struct CalcSurApp: App {
+struct MenuBarPopoverApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings{
+            SettingsView()
+        }
+    }
+}
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover = NSPopover.init()
+    var statusBarItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let contentView = ContentView()
+
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        popover.behavior = .transient // !!! - This does not seem to work in SwiftUI2.0 or macOS BigSur yet
+        popover.animates = false
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: contentView)
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        //statusBarItem?.button?.title = ""
+        statusBarItem?.button?.image = NSImage(systemSymbolName: "x.squareroot", accessibilityDescription: "") //Image(systemName: "x.squareroot")
+        statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+    }
+    @objc func showPopover(_ sender: AnyObject?) {
+        if let button = statusBarItem?.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+//            !!! - displays the popover window with an offset in x in macOS BigSur.
+        }
+    }
+    @objc func closePopover(_ sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
         }
     }
 }
--- CalcSur/ContentView.swift
+++ CalcSur/ContentView.swift
@@ -8,12 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstNumber = 0
+    @State private var secondNumber = 0
+    @State private var operand = ""
+    @State private var calculatorText = "0"
+    @State private var isTypingNumber = false
+    private func createCalcDigit(_ number: String) -> Button<Text> {
+        return Button(action: {
+            self.digitTapped(number)
+        }) {
+            (Text(number))
+        }
+    }
+    private func digitTapped(_ number: String) -> Void {
+        if isTypingNumber {
+            calculatorText += number
+        } else {
+            calculatorText = number
+            isTypingNumber = true
+        }
+    }
+    private func operandTapped(_ operand: String) {
+        isTypingNumber = false
+        firstNumber = Int(calculatorText)!
+        self.operand = operand
+        calculatorText = operand
+    }
+    private func calculate() {
+        isTypingNumber = false
+        var result  = 0
+        secondNumber = Int(calculatorText)!
+        if operand == "+" {
+            result = firstNumber + secondNumber
+        } else if operand == "-" {
+            result = firstNumber - secondNumber
+        }
+        else if operand == "*" {
+            result = firstNumber * secondNumber
+        }
+        else if operand == "/" {
+            if secondNumber == 0 {
+                result = 0
+            } else
+                {
+                result = firstNumber / secondNumber
+            }
+        }
+        calculatorText = "\(result)"
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("0", text: $calculatorText)
+                .border(Color.gray, width: 1)
+                .multilineTextAlignment(.trailing)
+                .frame(maxWidth:100)
+            HStack {
+                // 2.
+                createCalcDigit("1")
+                createCalcDigit("2")
+                createCalcDigit("3")
+            }
+            HStack {
+                createCalcDigit("4")
+                createCalcDigit("5")
+                createCalcDigit("6")
+            }
+            HStack {
+                createCalcDigit("7")
+                createCalcDigit("8")
+                createCalcDigit("9")
+            }
+            HStack {
+                
+                createCalcDigit("0")
+                Button(action: {
+                    self.operandTapped("+")
+                }) {
+                    (Text("+"))
+                }
+                Button(action: {
+                    self.operandTapped("*")
+                }) {
+                    (Text("*"))
+                }
+            }
+            HStack {
+                Button(action: {
+                    self.operandTapped("/")
+                }) {
+                    (Text("/"))
+                }
+                Button(action: {
+                    self.operandTapped("-")
+                }) {
+                    (Text("-"))
+                }
+                Button(action: {
+                    self.calculate()
+                }) {
+                    (Text("="))
+                }
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
