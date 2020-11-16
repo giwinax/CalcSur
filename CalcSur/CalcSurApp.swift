29cec2a3fdc98aa7a3b906757d82f5ed775bff29
sbi <giwinax@gmail.com>
2020-11-16 14:43:19 +0300

    Initial Commit

---
+++ CalcSur/CalcSurApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcSurApp.swift
+//  CalcSur
+//
+//  Created by s b on 16.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct CalcSurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
